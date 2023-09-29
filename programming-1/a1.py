81717176f60ccf62670038aa129499a088c54c51
jantiegges <jantiegges@gmail.com>
2023-09-29 10:53:09 -0400

    clean code and save results

--- programming-1/a1.py
+++ programming-1/a1.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import cross_val_score
 from itertools import product
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 
-import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
@@ -66,22 +65,38 @@ def feature_extraction(X_train, X_test, ngram_range=(1, 1)):
     return X_train_feat, X_test_feat
 
 
-def classification(X_train, y_train, classifier, cv=5,):
-    """Classify data using different classifier."""
+def classification(X_train, y_train, classifier, cv=5):
+    """Classify data using different classifier and return the best hyperparameters."""
 
+    # Define classifier and parameter grid for each case
     if classifier == 'svm':
         clf = SVC(kernel='linear')
+        param_grid = {
+            'C': [0.1, 1, 10, 100], # cost: smaller c --> wider margin
+        }
     elif classifier == 'logistic_regression':
-        clf = LogisticRegression()
+        clf = LogisticRegression(solver='saga', max_iter=5000)
+        param_grid = {
+            'C': [0.1, 0.5, 1, 5], #smoothing
+        }
     elif classifier == 'naive_bayes':
-        clf = MultinomialNB()
+        clf = MultinomialNB(force_alpha=True)
+        param_grid = {
+            'alpha': [0.1, 0.5, 1, 2]
+        }
     
-    cv_scores = cross_val_score(clf, X_train, y_train, cv=cv)
-    clf.fit(X_train, y_train)
+    # apply grid search with cross-validation
+    grid_search = GridSearchCV(clf, param_grid, cv=cv) 
+    grid_search.fit(X_train, y_train)
+    best_clf = grid_search.best_estimator_
 
-    return clf, cv_scores
+    # get score on the whole training set
+    train_score = best_clf.score(X_train, y_train)
 
-def plot_preprocessing_options_scores(results):
+    return best_clf, train_score, grid_search.best_params_
+
+
+def plot_table(results, title):
     """Plot the results of the preprocessing options."""
 
     df = pd.DataFrame(results)
@@ -94,9 +109,11 @@ def plot_preprocessing_options_scores(results):
     table.auto_set_font_size(False)
     table.set_fontsize(10)
     table.scale(1.5, 1.5)
+
+    plt.title(title, fontsize=14)
     
     # Export the plot as an image
-    plt.savefig('preprocessing_scores.png', bbox_inches='tight', dpi=300)
+    plt.savefig(f'{title}.png', bbox_inches='tight', dpi=300)
 
 
 if __name__ == '__main__':
@@ -105,7 +122,7 @@ if __name__ == '__main__':
     # data
     facts_file = 'data/facts.txt'
     fakes_file = 'data/fakes.txt'
-    test_size = 0.15
+    test_size = 0.2
 
     # feature extraction 
     ngram_range = (1, 1)
@@ -113,46 +130,99 @@ if __name__ == '__main__':
     # classification
     cv = 5
 
-    # Define the parameter combinations to test
+    # preprocessing options to compare
     preprocessing_options = list(product([True, False], repeat=3))
-
-    # Initialize a list to store the results
     results = []
+    best_preprocessing = {
+    'svm_best': {'cv_train_score': -1, 'test_score': -1, 'preprocessing_options': None, 'model_params': None},
+    'svm_worst': {'cv_train_score': 2, 'test_score': 2, 'preprocessing_options': None, 'model_params': None},
+    'logistic_regression_best': {'cv_train_score': -1, 'test_score': -1, 'preprocessing_options': None, 'model_params': None},
+    'logistic_regression_worst': {'cv_train_score': 2, 'test_score': 2, 'preprocessing_options': None, 'model_params': None},
+    'naive_bayes_best': {'cv_train_score': -1, 'test_score': -1, 'preprocessing_options': None, 'model_params': None},
+    'naive_bayes_worst': {'cv_train_score': 2, 'test_score': 2, 'preprocessing_options': None, 'model_params': None},
+    }
 
+    all_facts, labels = load_data(facts_file, fakes_file)
+
+    # run experiments for different preprocessing options
     for options in preprocessing_options:
 
+        # prep data
         remove_stopwords, stem, lemmatize = options
-        
-        # load data
-        all_facts, labels = load_data(facts_file, fakes_file)
         preprocessed_facts = [preprocess_text(fact, remove_stopwords, stem, lemmatize) for fact in all_facts]
         X_train, X_test, y_train, y_test = train_test_split(preprocessed_facts, labels, test_size=test_size, random_state=42)
         X_train, X_test = feature_extraction(X_train, X_test, ngram_range)
         
         # classification
-        clf_svm, cv_scores_svm = classification(X_train, y_train, 'svm', cv)
-        clf_lr, cv_scores_lr = classification(X_train, y_train, 'logistic_regression', cv)
-        clf_nb, cv_scores_nb = classification(X_train, y_train, 'naive_bayes', cv)
+        clf_svm, train_score_svm, svm_best_param = classification(X_train, y_train, 'svm', cv)
+        clf_lr, train_score_lr, lr_best_param = classification(X_train, y_train, 'logistic_regression', cv)
+        clf_nb, train_score_nb, nb_best_param = classification(X_train, y_train, 'naive_bayes', cv)
 
-        # calculate mean training error and test error
-        svm_train_score = round(cv_scores_svm.mean(), 3) 
+        # calculate scores
+        svm_train_score = round(train_score_svm, 3) 
+        svm_cv_train_score = round(np.mean(cross_val_score(clf_svm, X_train, y_train, cv=cv)), 3)
         svm_test_score = round(clf_svm.score(X_test, y_test), 3) 
 
-        lr_train_score = round(cv_scores_lr.mean(), 3)  
+        lr_train_score = round(train_score_lr, 3)  
+        lr_cv_train_score = round(np.mean(cross_val_score(clf_lr, X_train, y_train, cv=cv)), 3)
         lr_test_score = round(clf_lr.score(X_test, y_test), 3) 
 
-        nb_train_score = round(cv_scores_nb.mean(), 3) 
+        nb_train_score = round(train_score_nb, 3) 
+        nb_cv_train_score = round(np.mean(cross_val_score(clf_nb, X_train, y_train, cv=cv)), 3)
         nb_test_score = round(clf_nb.score(X_test, y_test), 3) 
 
+        # save all results
         results.append({
             'Stop rmv, Stem, Lemma': ', '.join(str(item) for item in options),
-            'NB Train/Test Score': f"{nb_train_score} / {nb_test_score}",
-            'LR Train/Test Score': f"{lr_train_score} / {lr_test_score}",
-            'SVM Train/Test Score': f"{svm_train_score} / {svm_test_score}"
+            'Train / CV Mean Train / Test': f"{nb_train_score} / {nb_cv_train_score} / {nb_test_score}",
+            'Train / CV Mean Train / Test': f"{lr_train_score} / {lr_cv_train_score} / {lr_test_score}",
+            'Train / CV Mean Train / Test': f"{svm_train_score} / {svm_cv_train_score} / {svm_test_score}"
         })
 
-    # plot the results
-    df = pd.DataFrame(results)
-    plot_preprocessing_options_scores(results)
+        # save best and worst results
+        if svm_cv_train_score > best_preprocessing['svm_best']['cv_train_score']:
+            best_preprocessing['svm_best']['model_params'] = svm_best_param
+            best_preprocessing['svm_best']['preprocessing_options'] = options
+            best_preprocessing['svm_best']['cv_train_score'] = svm_cv_train_score
+            best_preprocessing['svm_best']['test_score'] = svm_test_score
+        elif svm_cv_train_score < best_preprocessing['svm_worst']['cv_train_score']:
+            best_preprocessing['svm_worst']['model_params'] = svm_best_param
+            best_preprocessing['svm_worst']['preprocessing_options'] = options
+            best_preprocessing['svm_worst']['cv_train_score'] = svm_cv_train_score
+            best_preprocessing['svm_worst']['test_score'] = svm_test_score
 
-    # TODO: Print/plot more results
+        if lr_cv_train_score > best_preprocessing['logistic_regression_best']['cv_train_score']:
+            best_preprocessing['logistic_regression_best']['model_params'] = lr_best_param
+            best_preprocessing['logistic_regression_best']['preprocessing_options'] = options
+            best_preprocessing['logistic_regression_best']['cv_train_score'] = lr_cv_train_score
+            best_preprocessing['logistic_regression_best']['test_score'] = lr_test_score
+        elif lr_cv_train_score < best_preprocessing['logistic_regression_worst']['cv_train_score']:
+            best_preprocessing['logistic_regression_worst']['model_params'] = lr_best_param
+            best_preprocessing['logistic_regression_worst']['preprocessing_options'] = options
+            best_preprocessing['logistic_regression_worst']['cv_train_score'] = lr_cv_train_score
+            best_preprocessing['logistic_regression_worst']['test_score'] = lr_test_score
+
+        if nb_cv_train_score > best_preprocessing['naive_bayes_best']['cv_train_score']:
+            best_preprocessing['naive_bayes_best']['model_params'] = nb_best_param
+            best_preprocessing['naive_bayes_best']['preprocessing_options'] = options
+            best_preprocessing['naive_bayes_best']['cv_train_score'] = nb_cv_train_score
+            best_preprocessing['naive_bayes_best']['test_score'] = nb_test_score
+        elif nb_cv_train_score < best_preprocessing['naive_bayes_worst']['cv_train_score']:
+            best_preprocessing['naive_bayes_worst']['model_params'] = nb_best_param
+            best_preprocessing['naive_bayes_worst']['preprocessing_options'] = options
+            best_preprocessing['naive_bayes_worst']['cv_train_score'] = nb_cv_train_score
+            best_preprocessing['naive_bayes_worst']['test_score'] = nb_test_score
+
+        print(f"Stop rmv: {remove_stopwords}, Stem: {stem}, Lemma: {lemmatize}")
+        print(f"Best parameters for SVM: {svm_best_param}")
+        print(f"Best parameters for Logistic Regression: {lr_best_param}")
+        print(f"Best parameters for Naive Bayes: {nb_best_param}")
+        print("\n")
+
+    # plot a table for the best and worst preprocessing options
+    df_concise = pd.DataFrame(best_preprocessing)
+    plot_table(df_concise, title='Best and Worst Preprocessing Options')
+
+    # plot results from all preprocessing options
+    df = pd.DataFrame(results)
+    plot_table(results, title='Model Performance with Different Preprocessing Options')
